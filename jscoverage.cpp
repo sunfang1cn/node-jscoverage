abe63f850ba6fa2470069291bb2b8faecabd5420
sunfang1cn <sunfang1cn@yahoo.com.cn>
2012-02-23 21:28:14 +0800

    now it can be used...

--- jscoverage.cpp
+++ jscoverage.cpp
@@ -24,8 +24,10 @@
 #include <string.h>
 
 #include "global.h"
+extern "C" {
 #include "instrument.h"
 #include "instrument-js.h"
+}
 #include "resource-manager.h"
 #include "util.h"
 
@@ -51,122 +53,6 @@ Local<String> VAR = Local<String>::Cast(args[I]);
 
 const char * jscoverage_encoding = "ISO-8859-1";
 bool jscoverage_highlight = true;
-
-int main(int argc, char ** argv) {
-  int verbose = 0;
-
-  // program = argv[0];
-  program = "jscoverage";
-
-  char * source = NULL;
-  char * destination = NULL;
-
-  char ** no_instrument = xnew(char *, argc - 1);
-  int num_no_instrument = 0;
-
-  char ** exclude = xnew(char *, argc - 1);
-  int num_exclude = 0;
-  
-  jscoverage_highlight = false;
-
-  for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
-      copy_resource_to_stream("jscoverage-help.txt", stdout);
-      exit(EXIT_SUCCESS);
-    }
-    else if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0) {
-      version();
-    }
-    else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
-      verbose = 1;
-    }
-    else if (strcmp(argv[i], "--mozilla") == 0) {
-      jscoverage_mozilla = true;
-      jscoverage_set_js_version("180");
-    }
-    else if (strcmp(argv[i], "--no-instrument") == 0) {
-      i++;
-      if (i == argc) {
-        fatal_command_line("--no-instrument: option requires an argument");
-      }
-      no_instrument[num_no_instrument] = argv[i];
-      num_no_instrument++;
-    }
-    else if (strncmp(argv[i], "--no-instrument=", 16) == 0) {
-      no_instrument[num_no_instrument] = argv[i] + 16;
-      num_no_instrument++;
-    }
-    else if (strcmp(argv[i], "--exclude") == 0) {
-      i++;
-      if (i == argc) {
-        fatal_command_line("--exclude: option requires an argument");
-      }
-      exclude[num_exclude] = argv[i];
-      num_exclude++;
-    }
-    else if (strncmp(argv[i], "--exclude=", 10) == 0) {
-      exclude[num_exclude] = argv[i] + 10;
-      num_exclude++;
-    }
-    else if (strcmp(argv[i], "--encoding") == 0) {
-      i++;
-      if (i == argc) {
-        fatal_command_line("--encoding: option requires an argument");
-      }
-      jscoverage_encoding = argv[i];
-    }
-    else if (strncmp(argv[i], "--encoding=", 11) == 0) {
-      jscoverage_encoding = argv[i] + 11;
-    }
-    else if (strcmp(argv[i], "--js-version") == 0) {
-      i++;
-      if (i == argc) {
-        fatal_command_line("--js-version: option requires an argument");
-      }
-      jscoverage_set_js_version(argv[i]);
-    }
-    else if (strncmp(argv[i], "--js-version=", 13) == 0) {
-      jscoverage_set_js_version(argv[i] + 13);
-    }
-    else if (strncmp(argv[i], "-", 1) == 0) {
-      fatal_command_line("unrecognized option `%s'", argv[i]);
-    }
-    else if (source == NULL) {
-      source = argv[i];
-    }
-    else if (destination == NULL) {
-      destination = argv[i];
-    }
-    else {
-      fatal_command_line("too many arguments");
-    }
-  }
-
-  if (source == NULL || destination == NULL) {
-    fatal_command_line("missing argument");
-  }
-  
-  //exclude .svn .cvs .git dictionary
-  exclude[num_exclude] = ".svn";
-  exclude[num_exclude+1] = ".git";
-  exclude[num_exclude+2] = ".cvs";
-  num_exclude += 3;
-  
-  source = make_canonical_path(source);
-  destination = make_canonical_path(destination);
-
-  jscoverage_init();
-  jscoverage_instrument(source, destination, verbose, exclude, num_exclude, no_instrument, num_no_instrument);
-  jscoverage_cleanup();
-
-  free(source);
-  free(destination);
-  free(exclude);
-  free(no_instrument);
-
-  exit(EXIT_SUCCESS);
-}
-
 
 
 //for node v8
@@ -245,46 +131,155 @@ public:
 
     static Handle<Value> doSync(const Arguments& args)
     {
-        HandleScope scope;
-        
+      HandleScope scope;
+            
 
-        REQ_STR_ARG(0, source); String::AsciiValue _source(source);
-        REQ_STR_ARG(1, dest);   String::AsciiValue _dest(dest);
-        REQ_STR_ARG(2, exclude);String::AsciiValue _exclude(exclude);
-        REQ_STR_ARG(3, no_inst);String::AsciiValue _no_inst(no_inst);
-        REQ_STR_ARG(4, options);String::AsciiValue _options(options);
-        
-        Jscoverage* hw = ObjectWrap::Unwrap<Jscoverage>(args.This());
-        
-        char reqstr[50000] = {0};
-        char argv[100][300] = {{0}};
-        
-        strcat(reqstr, *_source);
-        strcat(reqstr, " ");
-        strcat(reqstr, *_dest);
-        strcat(reqstr, *_exclude);
-        strcat(reqstr, *_no_inst);
-        strcat(reqstr, *_options);
-        
-        strcpy(argv[0], "jscoverage");
-        int argc = 1;
-        char * p;
-        p = strtok (reqstr, " "); 
-        while(p!=NULL) { 
-            strcpy(argv[argc],p);;
-            argc++;
-            p = strtok(NULL, " "); 
-        } 
-        int ret;
-        try
-        {
-            ret = main(argc, (char **)argv);
+      REQ_STR_ARG(0, osource); String::AsciiValue _source(osource);
+      REQ_STR_ARG(1, dest);   String::AsciiValue _dest(dest);
+      REQ_STR_ARG(2, oexclude);String::AsciiValue _exclude(oexclude);
+      REQ_STR_ARG(3, no_inst);String::AsciiValue _no_inst(no_inst);
+      REQ_STR_ARG(4, ooptions);String::AsciiValue _options(ooptions);
+            
+      Jscoverage* hw = ObjectWrap::Unwrap<Jscoverage>(args.This());
+            
+      char reqstr[50000] = {0};
+      char **argv = (char **)malloc( 100 * sizeof(char *) );
+      for (int k=0;k<100;k++)
+         argv[k] = (char *)malloc( 300 * sizeof(char) );  
+            
+      strcat(reqstr, *_source);
+      strcat(reqstr, " ");
+      strcat(reqstr, *_dest);
+      strcat(reqstr, *_exclude);
+      strcat(reqstr, *_no_inst);
+      strcat(reqstr, *_options);
+
+      strcpy(argv[0], "jscoverage");
+      int argc = 1;
+      char * p;
+      p = strtok (reqstr, " "); 
+      while(p!=NULL) { 
+           strcpy(argv[argc],p);
+           argc++;
+           p = strtok(NULL, " "); 
+      } 
+      int ret = 0;
+            
+      int verbose = 0;
+      
+      // program = argv[0];
+      program = "jscoverage";
+
+      char * source = NULL;
+      char * destination = NULL;
+
+      char ** no_instrument = xnew(char *, argc - 1);
+      int num_no_instrument = 0;
+
+      char ** exclude = xnew(char *, argc - 1);
+      int num_exclude = 0;
+      
+      jscoverage_highlight = false;
+
+      for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+          copy_resource_to_stream("jscoverage-help.txt", stdout);
+          exit(EXIT_SUCCESS);
         }
-        catch(Exception * e)
-        {
-            ret = -1;
+        else if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0) {
+          version();
         }
-        return scope.Close(Number::New(ret));
+        else if (strcmp(argv[i], "-v") == 0 || strcmp(argv[i], "--verbose") == 0) {
+          verbose = 1;
+        }
+        else if (strcmp(argv[i], "--mozilla") == 0) {
+          jscoverage_mozilla = true;
+          jscoverage_set_js_version("180");
+        }
+        else if (strcmp(argv[i], "--no-instrument") == 0) {
+          i++;
+          if (i == argc) {
+            fatal_command_line("--no-instrument: option requires an argument");
+          }
+          no_instrument[num_no_instrument] = argv[i];
+          num_no_instrument++;
+        }
+        else if (strncmp(argv[i], "--no-instrument=", 16) == 0) {
+          no_instrument[num_no_instrument] = argv[i] + 16;
+          num_no_instrument++;
+        }
+        else if (strcmp(argv[i], "--exclude") == 0) {
+          i++;
+          if (i == argc) {
+            fatal_command_line("--exclude: option requires an argument");
+          }
+          exclude[num_exclude] = argv[i];
+          num_exclude++;
+        }
+        else if (strncmp(argv[i], "--exclude=", 10) == 0) {
+          exclude[num_exclude] = argv[i] + 10;
+          num_exclude++;
+        }
+        else if (strcmp(argv[i], "--encoding") == 0) {
+          i++;
+          if (i == argc) {
+            fatal_command_line("--encoding: option requires an argument");
+          }
+          jscoverage_encoding = argv[i];
+        }
+        else if (strncmp(argv[i], "--encoding=", 11) == 0) {
+          jscoverage_encoding = argv[i] + 11;
+        }
+        else if (strcmp(argv[i], "--js-version") == 0) {
+          i++;
+          if (i == argc) {
+            fatal_command_line("--js-version: option requires an argument");
+          }
+          jscoverage_set_js_version(argv[i]);
+        }
+        else if (strncmp(argv[i], "--js-version=", 13) == 0) {
+          jscoverage_set_js_version(argv[i] + 13);
+        }
+        else if (strncmp(argv[i], "-", 1) == 0) {
+          fatal_command_line("unrecognized option `%s'", argv[i]);
+        }
+        else if (source == NULL) {
+          source = argv[i];
+        }
+        else if (destination == NULL) {
+          destination = argv[i];
+        }
+        else {
+          fatal_command_line("too many arguments");
+        }
+      }
+
+      if (source == NULL || destination == NULL) {
+        fatal_command_line("missing argument");
+      }
+      
+      //exclude .svn .cvs .git dictionary
+      exclude[num_exclude] = ".svn";
+      exclude[num_exclude+1] = ".git";
+      exclude[num_exclude+2] = ".cvs";
+      num_exclude += 3;
+      
+      source = make_canonical_path(source);
+      destination = make_canonical_path(destination);
+
+      jscoverage_init();
+      jscoverage_instrument(source, destination, verbose, exclude, num_exclude, no_instrument, num_no_instrument);
+      jscoverage_cleanup();
+
+      free(source);
+      free(destination);
+      free(exclude);
+      free(no_instrument);
+
+      exit(EXIT_SUCCESS);
+        
+      free(argv);
+      return scope.Close(Number::New(ret));
     }
 
 };
