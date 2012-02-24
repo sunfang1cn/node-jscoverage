1a9ebfccced31449af5bb4f403bcad68b848a276
sunfang1cn <sunfang1cn@yahoo.com.cn>
2012-02-24 15:53:52 +0800

    fix auto build script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+NODE_INCLUDE='/usr/include/nodejs'
 ./configure
-g++  -DHAVE_CONFIG_H -I.    -Ijs -Ijs/obj -DXP_UNIX -g -O2 -I /usr/include/nodejs -g -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -Wall -fPIC -MT jscoverage.o -MD -MP -MF .deps/jscoverage.Tpo -c -o jscoverage.o jscoverage.cpp
+make
+g++  -DHAVE_CONFIG_H -I.    -Ijs -Ijs/obj -DXP_UNIX -g -O2 -I $NODE_INCLUDE -g -D_FILE_OFFSET_BITS=64 -D_LARGEFILE_SOURCE -Wall -fPIC -MT jscoverage.o -MD -MP -MF .deps/jscoverage.Tpo -c -o jscoverage.o jscoverage.cpp
 make
