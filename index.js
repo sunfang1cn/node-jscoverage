e8e19c18e7a5199c58070301a3138882020fbe4e
sunfang1cn <sunfang1cn@yahoo.com.cn>
2012-02-26 23:51:09 +0800

    fix bugs for processSingle and divide it into two functions

--- index.js
+++ index.js
@@ -8,36 +8,54 @@ var fs = require('fs');
 var _jscoverage = require('./jscoverage.node')
 var _jsclass = new _jscoverage.Jscoverage();
 
-exports.processSingle = function(filename, content, encode) {
-
-  if(content!=null && typeof(content)==='Object' && encode!=null) 
-    var _content = content;
-  if(content!=null && typeof(content)==='string' && encode==null)
-    var _encode = content;
+exports.process = function(filename, content, encode) {
   if(encode!=null && typeof(encode)==='string')
     var _encode = encode;
+  else
+    var _encode = 'utf-8';
+  
+
+  if(content!=null && Buffer.isBuffer(content)) 
+    var _content = content;
+  if(content!=null && typeof(content)==='string')
+    var _content = new Buffer(content, _encode);
+
+  if(_content==null) {
+    console.error('content is required.');
+    return null;
+  }
   if(filename==null || typeof(filename)!='string') {
     console.error('filename is required.');
     return null;
   }
-  if(_encode==null)  var _encode = 'utf-8';
- 
-  if(_content!=null) {
-    var _buf = _jsclass.doBufferSync(_content, _encode, filename);
-    return _buf.toSting(_encode);
-  }else{
-    var _stats = fs.statSync(filename);
-    if(!_stats.isFile()) {
-      console.error('file is not correct.');
-      return null;
-    }
-    var _buffer = new Buffer(_stats.size);
-    var _fd = fs.openSync(filename, 'r');
-    fs.readSync(_fd, _buffer, 0, _stats.size, 0);
-    var _buf = _jsclass.doBufferSync(_buffer, _encode, filename);
-    return _buf.toString(_encode);
-  }
+
+  var _buf = _jsclass.doBufferSync(_content, _encode, filename);
+  return _buf.toString(_encode);
 };
+
+exports.processFile = function(filename, encode) {
+  if(encode!=null && typeof(encode)==='string')
+    var _encode = encode;
+  else
+    var _encode = 'utf-8';
+
+  if(filename==null || typeof(filename)!='string') {
+    console.error('filename is required.');
+    return null;
+  }
+  
+  var _stats = fs.statSync(filename);
+  if(!_stats.isFile()) {
+    console.error('file is not correct.');
+    return null;
+  }
+  var _buffer = new Buffer(_stats.size);
+  var _fd = fs.openSync(filename, 'r');
+  fs.readSync(_fd, _buffer, 0, _stats.size, 0);
+  var _buf = _jsclass.doBufferSync(_buffer, _encode, filename);
+  return _buf.toString(_encode);	
+}
+
 
 exports.processDir = function(source, dest, exclude, exclude_file, options) {
   if(typeof(source)!='string' || typeof(dest)!='string') {
