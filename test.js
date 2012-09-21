03c0ff3cc86e382cbdf1633cc88ccc57733f4e3b
fish <zhengxinlin@gmail.com>
2012-09-21 14:25:52 +0800

    #add _reset function

---
+++ test.js
@@ -0,0 +1,20 @@
+expect = require('expect.js');
+var jsc = require('./index');
+require = jsc.mock(module);
+var testCase = require('./test_case.js',true);
+
+describe("jscoverage",function(){
+  it('#_rest()',function(){
+    testCase._replace('conn',function(cfg){
+      return db.close(cfg);
+    });
+    var res = testCase._call('conn',[{}]);
+    expect(res).to.be('close');
+  });
+  it('#_replace()',function(){
+    testCase._reset('conn');
+    var conn = testCase._get('conn');
+    var res = conn({})
+    expect(res).to.be('conn');
+  });
+});
